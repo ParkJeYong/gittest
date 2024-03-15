ff3021dd38f02b0394de337b96dafb45bfd7db3f
JeYong <pjy3306@naver.com>
2024-03-15 11:19:34 +0900

    깃 수정

--- abc/src/main/java/testabc/AbcTest.java
+++ abc/src/main/java/testabc/AbcTest.java
@@ -3,7 +3,7 @@ package testabc;
 public class AbcTest {
 
 	public static void main(String[] args) {
-		System.out.println("깃 연습");
+		System.out.println("깃 커밋 연습");
 		// TODO Auto-generated method stub
 
 	}
